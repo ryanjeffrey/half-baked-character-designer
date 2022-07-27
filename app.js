40204de8d29c5ccb9f87d7f2ae79a76eb1d6fa68
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-07-27 17:01:55 -0500

    Increment counts for state changes

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ headDropdown.addEventListener('change', () => {
     // get the value of the head dropdown
 
     // increment the head change count state
-    
+    timesHeadChanged++;
     // update the dom for the head (use style.backgroundImage on the headEl div instead of trying to set the .src -- it's NOT an img tag!)
     headEl.style.backgroundImage = `url(assets/${headDropdown.value}-head.png)`;
     // update the stats to show the new count (call displayStats() to do this work)
@@ -32,7 +32,7 @@ middleDropdown.addEventListener('change', () => {
     // get the value of the middle dropdown
 
     // increment the middle change count state
-    
+    timesMiddleChanged++;
     // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
     middleEl.style.backgroundImage = `url(assets/${middleDropdown.value}-middle.png)`;
     // update the stats to show the new count (call displayStats() to do this work)
@@ -43,7 +43,7 @@ bottomDropdown.addEventListener('change', () => {
     // get the value of the bottom dropdown
 
     // increment the bottom change count state
-    
+    timesBottomChanged++;
     // update the dom for the bottom (NOTE use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
     bottomEl.style.backgroundImage = `url(assets/${bottomDropdown.value}-pants.png)`;
     // update the stats to show the new count (call displayStats() to do this work)
